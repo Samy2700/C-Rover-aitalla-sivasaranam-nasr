a5835499338d9597cde2e22af4244ca28cd457eb
ilies <ilies.nasr@efrei.net>
2024-11-12 16:43:21 +0100

    changement de <malloc.h> à <stdlib.h> par souci de compatibilité

--- MARC-master/cmake-build-debug/CMakeFiles/untitled1.dir/moves.c.obj.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/untitled1.dir/moves.c.obj: \
- C:\Users\samya\MARC-master\MARC-master\moves.c \
- C:\Users\samya\MARC-master\MARC-master\moves.h \
- C:\Users\samya\MARC-master\MARC-master\loc.h
